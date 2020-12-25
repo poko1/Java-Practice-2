551371f7e48faf51001e6e2971674c2bf410f203
poko1 <42430946+poko1@users.noreply.github.com>
2020-12-25 19:08:59 +0600

    Create JavaApplication.java

---
+++ JavaApplication.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+/**
+ *
+ * @author Maliha
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public int addNumbers(int a, int b) {
+    int sum = a + b;
+    // return value
+    return sum;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 = 25;
+    int num2 = 15;
+
+    // create an object of Main
+    JavaApplication obj = new JavaApplication();
+    // calling method
+    int result = obj.addNumbers(num1, num2);
+    System.out.println("Sum is: " + result);
+    }
+
+}
